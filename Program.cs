0905985d039d95bdbe3ae6235766167ea2cac9aa
GabrielHertal <gabrielhertal@hotmail.com>
2023-12-14 13:29:52 -0300

    tela de login

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace Gerenciamento_de_Biblioteca
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Form_Login());
         }
     }
 }
